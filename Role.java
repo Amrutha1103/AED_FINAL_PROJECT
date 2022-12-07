490c031bd1099b077ce89b3c110bf25ca42fdfbb
Amrutha1103 <114834509+Amrutha1103@users.noreply.github.com>
2022-12-07 17:21:42 -0500

    Add files via upload

--- Role.java
+++ Role.java
@@ -4,10 +4,48 @@
  */
 package Schema.Role;
 
+import Schema.EcoSystem;
+import Schema.Organization.Organization;
+import Schema.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author amruthaedara
  */
-public class Role {
+public abstract class Role {
+    
+    public enum RoleType{
+        Admin("Admin"), 
+        Doctor("Doctor"),
+        Logistics("Logistics"), 
+        Pharmacist("Pharmacist");
+        
+        private String value;
+        private RoleType(String value){
+            this.value = value;
+            }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+        
+        @Override 
+        public String toString(){
+            return value;
+        }
+    }
+    
+    public abstract JPanel createWorkArea (JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network);
+    @Override
+    public String toString(){
+        return this.getClass().getName();
+    }
+    
+ 
     
 }
