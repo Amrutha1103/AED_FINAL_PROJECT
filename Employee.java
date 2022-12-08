e907997bcf447c51cece6acdff0ecf2327cb006d
Amrutha1103 <114834509+Amrutha1103@users.noreply.github.com>
2022-12-07 20:50:38 -0500

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -9,5 +9,29 @@ package Schema.Employee;
  * @author amruthaedara
  */
 public class Employee {
+    private String EmployeeName;
+    private int EmployeeId;
+    private static int count = 1;
+    
+    
+    public Employee() {
+        EmployeeId = count;
+        count++;
+    }
+    
+    public int getEmployeeId(){
+        return EmployeeId;
+    }
+    public void setName (String EmployeeName){
+        this.EmployeeName=EmployeeName;
+        }
+    public String getEmployeeName(){
+        return EmployeeName;
+        }
+    
+    @Override
+    public String toString(){
+        return EmployeeName;
+    }
     
 }
