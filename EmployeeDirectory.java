c0e528cbe59030ce4a4454fa1699efff48e74276
Amrutha1103 <114834509+Amrutha1103@users.noreply.github.com>
2022-12-07 20:56:46 -0500

    Add files via upload

--- EmployeeDirectory.java
+++ EmployeeDirectory.java
@@ -4,10 +4,30 @@
  */
 package Schema.Employee;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author amruthaedara
  */
 public class EmployeeDirectory {
+    
+    private ArrayList<Employee> employeeList;
+   
+    public EmployeeDirectory (){
+        employeeList = new ArrayList<>();
+        }
+    
+    
+    public ArrayList<Employee> getEmployeeList(){
+        return employeeList;
+    }
+    
+    public Employee createEmployee(String EmployeeName) {
+        Employee employee = new Employee();
+        employee.setName(EmployeeName);
+        employeeList.add(employee);
+        return employee;
+    }
     
 }
