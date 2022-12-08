46d2e9cc28253a894bb53848c6ee052957d97955
Amrutha1103 <114834509+Amrutha1103@users.noreply.github.com>
2022-12-07 20:26:50 -0500

    Add files via upload

--- MedicalEquipmentRole.java
+++ MedicalEquipmentRole.java
@@ -15,5 +15,17 @@ import javax.swing.JPanel;
  */
 public class MedicalEquipmentRole extends Role {
 
-    
+@Override
+public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business,Network network) {
+return new HealthcareEquipmentWorkAreaJPanel(userProcessContainer, business, account,  organization,network);
+}
+
+
+
+@Override
+public String toString() {
+    return "Medical Equipment Role";
+}
+
+
 }
