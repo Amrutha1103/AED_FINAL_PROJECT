589569aff7e5d43f480629e9e7cf08344570eceb
Amrutha1103 <114834509+Amrutha1103@users.noreply.github.com>
2022-12-07 21:33:07 -0500

    Add files via upload

--- DoctorPrescription.java
+++ DoctorPrescription.java
@@ -4,10 +4,69 @@
  */
 package Schema.Doctor;
 
+import java.util.Date;
+
 /**
  *
  * @author amruthaedara
  */
 public class DoctorPrescription {
+    
+    private String NameOfMedicine;
+    private int NoOfDays;
+    private int TimesInADay;
+    private String daignosis;
+    
+    public String getDaignosis() {
+        return daignosis;
+    }
+
+    public void setDaignosis(String daignosis) {
+        this.daignosis = daignosis;
+    }
+    private Date DateOfPrescription;
+    private String networkName;
+
+    public String getNameOfMedicine() {
+        return NameOfMedicine;
+    }
+
+    public void setNameOfMedicine(String NameOfMedicine) {
+        this.NameOfMedicine = NameOfMedicine;
+    }
+
+    public int getNoOfDays() {
+        return NoOfDays;
+    }
+
+    public void setNoOfDays(int NoOfDays) {
+        this.NoOfDays = NoOfDays;
+    }
+
+    public int getTimesInADay() {
+        return TimesInADay;
+    }
+
+    public void setTimesInADay(int TimesInADay) {
+        this.TimesInADay = TimesInADay;
+    }
+
+    public Date getDateOfPrescription() {
+        return DateOfPrescription;
+    }
+
+    public void setDateOfPrescription(Date DateOfPrescription) {
+        this.DateOfPrescription = DateOfPrescription;
+    }
+
+    public String getNetworkName() {
+        return networkName;
+    }
+
+    public void setNetworkName(String networkName) {
+        this.networkName = networkName;
+    }
+    
+    
     
 }
