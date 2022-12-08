3ce0f8fd9c76fae5868a913059f61eeee748992d
Amrutha1103 <114834509+Amrutha1103@users.noreply.github.com>
2022-12-07 20:26:11 -0500

    Add files via upload

---
+++ MedicalEquipmentRole.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Schema.Role;
+
+import Schema.EcoSystem;
+import Schema.Organization.Organization;
+import Schema.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author amruthaedara
+ */
+public class MedicalEquipmentRole extends Role {
+
+    
+}
