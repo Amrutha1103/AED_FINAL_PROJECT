70dbd060a78f51a15359bdb612510443b721e66f
AnusreeMyneni <114777596+AnusreeMyneni@users.noreply.github.com>
2022-12-10 00:55:59 -0500

    Add files via upload

---
+++ PharmacyWorkRequest.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Schema.WorkQueue;
+
+import java.util.Date;
+
+/**
+ *
+ * @author asr
+ */
+public class PharmacyWorkRequest extends WorkRequest {
+    private String requestDate;
+    private String status ;
+    private String medicationName;
+    private int Quantity;
+    private String network;
+    private String supplierName;
+
+    public String getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMedicationName() {
+        return medicationName;
+    }
+
+    public void setMedicationName(String medicationName) {
+        this.medicationName = medicationName;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int Quantity) {
+        this.Quantity = Quantity;
+    }
+
+    public String getNetwork() {
+        return network;
+    }
+
+    public void setNetwork(String network) {
+        this.network = network;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+    
+    @Override
+    public String toString(){
+        return this.medicationName;
+    }
+}
