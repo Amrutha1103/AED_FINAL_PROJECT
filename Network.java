8469ccf9bd89c38a14a68cf7130edc1f89079bc8
Amrutha1103 <114834509+Amrutha1103@users.noreply.github.com>
2022-12-09 20:14:47 -0500

    Add files via upload

--- Network.java
+++ Network.java
@@ -1,13 +1,44 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Schema.Network;
+
+import Schema.Enterprise.EnterpriseDirectory;
 
 /**
  *
  * @author amruthaedara
  */
 public class Network {
-    
+private String name;
+private EnterpriseDirectory enterpriseDirectory;
+
+public Network(){
+    enterpriseDirectory = new EnterpriseDirectory();
 }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+
+    public void setEnterpriseDirectory(EnterpriseDirectory enterpriseDirectory) {
+        this.enterpriseDirectory = enterpriseDirectory;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
