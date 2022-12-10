a222892ed00fdd83dddaf6d76727213db631a47f
AnusreeMyneni <114777596+AnusreeMyneni@users.noreply.github.com>
2022-12-10 00:55:31 -0500

    Add files via upload

---
+++ DoctorWorkRequest.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Schema.WorkQueue;
+
+import Schema.UserAccount.UserAccount;
+
+/**
+ *
+ * @author asr
+ */
+import java.util.Date;
+
+public class DoctorWorkRequest {
+    private UserAccount sender;
+    private UserAccount receiver;
+    private String medicationName;
+    private String patientName;
+    private String hospitalName;
+    private String status;
+    private Date requestDate;
+    private int Quantity;
+
+    public UserAccount getSender() {
+        return sender;
+    }
+
+    public void setSender(UserAccount sender) {
+        this.sender = sender;
+    }
+
+    public UserAccount getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(UserAccount receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getMedicationName() {
+        return medicationName;
+    }
+
+    public void setMedicationName(String medicationName) {
+        this.medicationName = medicationName;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int Quantity) {
+        this.Quantity = Quantity;
+    }
+    
+    public DoctorWorkRequest(){
+        requestDate = new Date();
+    }
+    
+}
